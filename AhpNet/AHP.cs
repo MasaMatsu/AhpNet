7659ea2f4107dc410f829eeed8b0e37989c3a1d1
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-08-26 15:53:14 +0900

    Add Test method for DLL debugging

--- AhpNet/AHP.cs
+++ AhpNet/AHP.cs
@@ -4,5 +4,13 @@ namespace AhpNet
 {
     public class AHP
     {
+        /// <summary>
+        /// Test Method for DLL debugging
+        /// </summary>
+        /// <returns></returns>
+        public static string HelloLibrary()
+        {
+            return "Hello World.";
+        }
     }
 }
