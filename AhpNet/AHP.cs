abec908209e8091d709b54aa7f5e7113a5266fef
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-08-26 22:05:00 +0900

    AHP: Add Result

--- AhpNet/AHP.cs
+++ AhpNet/AHP.cs
@@ -29,6 +29,22 @@ namespace AhpNet
             MPCEAs = mpceas;
         }
 
+        public Vector<T> Result() // The weight of Problem-Evaluations
+        {
+            var wpe = MPCPE.MaxEigen().Vector;
+
+            var rows = MPCEAs.First().ColumnCount;
+            var columns = MPCEAs.Count();
+            var wea = Matrix<T>.Build.Dense(rows, columns); // The weight of Evaluations-Alternatives
+            foreach (var x in MPCEAs.Select((mpcea, index) => (mpcea, index)))
+            {
+                var vector = x.mpcea.MaxEigen().Vector;
+                wea.SetColumn(x.index, vector);
+            }
+
+            return wea * wpe;
+        }
+
         /// <summary>
         /// Test Method for DLL debugging
         /// </summary>
