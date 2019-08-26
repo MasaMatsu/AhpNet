b0589a0415da87fffad1a64396fa1375eb77ebd9
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-08-26 18:27:30 +0900

    AHP: Add properties

--- AhpNet/AHP.cs
+++ AhpNet/AHP.cs
@@ -1,9 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace AhpNet
 {
-    public class AHP
+    public class AHP<T> where T : struct, IEquatable<T>, IFormattable
     {
+        #region Properties
+
+        /// <summary>
+        /// The matrix for pairwise comparison of Problem-Evaluations
+        /// </summary>
+        public Matrix<T> MPCPE { get; private set; }
+
+        /// <summary>
+        /// The matrix for pairwise comparison of Evaluations-Alternatives
+        /// </summary>
+        public IEnumerable<Matrix<T>> MPCEAs { get; private set; }
+
+        #endregion
+
         /// <summary>
         /// Test Method for DLL debugging
         /// </summary>
