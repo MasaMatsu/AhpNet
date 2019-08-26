9e3f3fbc014dd04e45afd7d1f31bc1aee7e26f35
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-08-26 23:35:57 +0900

    Add ResultTest

--- AhpNetTest/AHPTest.cs
+++ AhpNetTest/AHPTest.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathNet.Numerics.LinearAlgebra;
 
 using AhpNet;
 
@@ -7,5 +9,34 @@ namespace AhpNetTest
     [TestClass]
     public class AHPTest
     {
+        [TestMethod]
+        public void ResultTest()
+        {
+            var wpe = Matrix<double>.Build.DenseOfArray(new double[,]{
+                {1d,    3d, 1d/3d},
+                {1d/3d, 1d, 1d/5d},
+                {3d,    5d, 1d},
+            });
+            var weas = new List<Matrix<double>> {
+                Matrix<double>.Build.DenseOfArray(new double[,]{
+                    {1d,    5d, 1d/3d},
+                    {1d/5d, 1d, 1d/7d},
+                    {3d,    7d, 1d},
+                }),
+                Matrix<double>.Build.DenseOfArray(new double[,]{
+                    {1d, 1d/3d, 1d/5d},
+                    {3d, 1d,    1d/3d},
+                    {5d, 3d,    1d},
+                }),
+                Matrix<double>.Build.DenseOfArray(new double[,]{
+                    {1d, 1d/5d, 1d/3d},
+                    {5d, 1d,    3d},
+                    {3d, 1d/3d, 1d},
+                }),
+            };
+            var ahp = new AHP<double>(wpe, weas);
+            var result = ahp.Result();
+            Assert.AreEqual(1, 1);
+        }
     }
 }
