1eb80eee04b67c6f87208e1b192f06f4e73a2cf4
MasaMatsu <27629103+MasaMatsu@users.noreply.github.com>
2019-08-26 18:13:51 +0900

    Add Matrix extensions

--- AhpNet/Extensions.cs
+++ AhpNet/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Numerics;
 using MathNet.Numerics.LinearAlgebra;
 
@@ -6,6 +7,34 @@ namespace AhpNet
 {
     public static class Extensions
     {
-        // TODO: Write
+        #region Matrix<T>
+
+        /// <summary>
+        /// Get maximum eigen value and its eigen vector.
+        /// </summary>
+        public static (Complex Value, Vector<T> Vector) MaxEigen<T>(this Matrix<T> matrix) where T : struct, IEquatable<T>, IFormattable
+        {
+            var eigen = matrix.Evd();
+            var maxEigenValueIndexed =
+                eigen.EigenValues
+                .EnumerateIndexed()
+                .Select(x => (Index: x.Item1, Value: x.Item2.Real))
+                .OrderByDescending(x => x.Value)
+                .First();
+            var maxEigenVector = eigen.EigenVectors.Column(maxEigenValueIndexed.Index);
+            return (maxEigenValueIndexed.Value, maxEigenVector);
+        }
+
+        /// <summary>
+        /// Get Consistency Index(C.I) of the Matrix.
+        /// </summary>
+        public static double CI<T>(this Matrix<T> matrix) where T : struct, IEquatable<T>, IFormattable
+        {
+            var eigenValue = matrix.MaxEigen().Value.Real;
+            var count = (double)matrix.ColumnCount;
+            return (eigenValue - count) / (count - 1);
+        }
+
+        #endregion
     }
 }
